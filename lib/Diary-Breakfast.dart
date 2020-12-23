8bbbbb08a759c6bd7f6ae924bba7dd0bf079a2f4
Wenpeng778435592 <wzho530@aucklanduni.ac.nz>
2020-12-24 01:06:14 +1300

    Update Diary-Breakfast.dart

--- lib/Diary-Breakfast.dart
+++ lib/Diary-Breakfast.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import "package:my_diet_diary/qr_view/scan_view.dart";
+import "package:my_diet_diary/qr_view/page_search.dart";
 
 class Breakfast_Section extends StatefulWidget {
   @override
@@ -31,7 +34,13 @@ class _Breakfast_SectionState extends State<Breakfast_Section> {
             Text('Breakfast'),
             IconButton(
               icon:FaIcon(FontAwesomeIcons.barcode),
-              onPressed: (){},
+              onPressed: ()async {
+                PermissionStatus _hasPermission = await Permission.camera.request();
+                if(!_hasPermission.isGranted) return;
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ScanView()),
+                );
+              },
             ),
 
 
@@ -69,15 +78,26 @@ class _Breakfast_SectionState extends State<Breakfast_Section> {
             children: <Widget>[
               Expanded(
                 child: RaisedButton(
-                  onPressed: (){},
-                  child:Text('Find a Food',
-                    style: generalStyle,
+                  onPressed: (){
+                    Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => NeteaseSearchPage()),
+                    );
+                  },
+                  child:Row(
+
+                    children: [
+                      Text('Find a Food',
+                        style: generalStyle,
+                      ),
+                      Icon(Icons.search)
+                    ],
                   ),
                   color: Colors.amber,
                 ),
               ),
             ],
           ),
+
 
 
         ],
