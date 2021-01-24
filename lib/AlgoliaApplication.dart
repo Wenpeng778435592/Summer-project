c8e8f216c6853ad401807385d98560af75bedd3a
Wenpeng778435592 <wzho530@aucklanduni.ac.nz>
2021-01-24 19:14:25 +1300

    linking firebase and algolia

---
+++ lib/AlgoliaApplication.dart
@@ -0,0 +1,7 @@
+import 'package:algolia/algolia.dart';
+
+class AlgoliaApplication{
+  static final Algolia algolia = Algolia.init(
+      applicationId: "ZDYLDI0MXO",
+      apiKey: "64ad0d3afe63a8bea4101ba433d17285",);
+}
