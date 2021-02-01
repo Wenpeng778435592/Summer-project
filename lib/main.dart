7979677a2662102998c24f71a5cf8d7a2561f5c0
Wenpeng778435592 <69614012+Wenpeng778435592@users.noreply.github.com>
2021-02-02 02:18:27 +1300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_diet_diary/Diary.dart';
-import 'package:my_diet_diary/Weight.dart';
-import 'package:my_diet_diary/QuickAdd.dart';
 import 'package:my_diet_diary/Energy.dart';
 import 'package:my_diet_diary/More.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:my_diet_diary/QuickAdd.dart';
+import 'package:my_diet_diary/Weight.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'User_input/GeneralInfo.dart';
 
 
 final databaseReference = FirebaseDatabase().reference().reference();
 
-main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  int userID = prefs.getInt("currentUserID");
+
+  print(userID.toString());
+
   runApp(MaterialApp(
-    // If current user doesn't exists, show profile screen to create new user
-      home: Home()
-  ));
+      // If current user doesn't exist, show profile screen to create new user
+      home: userID == null ? GeneralInfo_Section() : Home()));
 }
 
 class Home extends StatefulWidget {
@@ -35,8 +44,6 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-
       body: _options.elementAt(_currentIndex),
 
 
@@ -47,34 +54,31 @@ class _HomeState extends State<Home> {
         iconSize: 35,
         items: [
           BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.book),
-              label: 'Diary',
+            icon: FaIcon(FontAwesomeIcons.book),
+            label: 'Diary',
           ),
           BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.weight),
-              label:'Weight',
+            icon: FaIcon(FontAwesomeIcons.weight),
+            label: 'Weight',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label:'QuickAdd',
+            icon: Icon(Icons.add),
+            label: 'QuickAdd',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.fastfood),
-              label:'Energy',
+            icon: Icon(Icons.fastfood),
+            label: 'Energy',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.help),
-              label:'More',
+            icon: Icon(Icons.help),
+            label: 'More',
           ),
-
         ],
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
-
         },
-
       ),
     );
   }
@@ -83,5 +87,3 @@ class _HomeState extends State<Home> {
     // row to insert
   }
 }
-
-
