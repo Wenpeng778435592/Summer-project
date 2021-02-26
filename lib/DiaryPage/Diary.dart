7ea1304e1be0e805fe778043610f79ff9760f7c4
Emily Zou <emilyzou98@gmail.com>
2021-02-26 17:17:24 +1300

    refactor progress bar

--- lib/DiaryPage/Diary.dart
+++ lib/DiaryPage/Diary.dart
@@ -1,5 +1,3 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 import 'package:my_diet_diary/DataObjects/DatabaseHelper.dart';
 import 'package:my_diet_diary/DataObjects/FoodEntry.dart';
@@ -8,6 +6,7 @@ import 'package:my_diet_diary/Diary-Breakfast.dart';
 import 'package:my_diet_diary/Diary-Dinner.dart';
 import 'package:my_diet_diary/Diary-Lunch.dart';
 import 'package:my_diet_diary/Diary-Snack.dart';
+import 'package:my_diet_diary/DiaryPage/CalorieProgressBar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'CustomExpansionTile.dart';
@@ -18,7 +17,7 @@ class Dairy_Section extends StatefulWidget {
 }
 
 class _Dairy_SectionState extends State<Dairy_Section> {
-  static TextStyle generalStyle = TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.grey[800]);
+  static TextStyle generalStyle = TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white);
 
   Future _futures;
 
@@ -94,31 +93,6 @@ class _Dairy_SectionState extends State<Dairy_Section> {
     });
   }
 
-  Widget _getCalorieDifferenceText() {
-    var color;
-    var text;
-
-    if (_targetCalories > _totalCalories) {
-      text = (_targetCalories - _totalCalories).toInt().toString() + " kcal left";
-      color = Colors.grey[500];
-    } else {
-      text = (_totalCalories - _targetCalories).toInt().toString() + " kcal over";
-      color = Colors.red[300];
-    }
-
-    return Text(text, style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: color));
-  }
-
-  _getCaloriesForMeal(List<FoodEntry> mealEntries) {
-    double calories = 0;
-
-    mealEntries.forEach((FoodEntry foodEntry) {
-      calories += foodEntry.calories;
-    });
-
-    return calories;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -152,91 +126,33 @@ class _Dairy_SectionState extends State<Dairy_Section> {
                   padding: EdgeInsets.fromLTRB(10, 50, 10, 10),
                   child: Column(
                     children: <Widget>[
-                      Container(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                          child: Stack(
-                            children: [
-                              CustomPaint(
-                                size: Size(280, 130),
-                                painter: ProgressArc(null, null, Colors.grey[300]),
-                              ),
-                              CustomPaint(
-                                size: Size(300, 150),
-                                painter: ProgressArc(
-                                    _getCaloriesForMeal(_breakfastToday) +
-                                        _getCaloriesForMeal(_lunchToday) +
-                                        _getCaloriesForMeal(_dinnerToday) +
-                                        _getCaloriesForMeal(_snackToday),
-                                    _targetCalories,
-                                    Colors.green[300]),
-                              ),
-                              CustomPaint(
-                                size: Size(300, 150),
-                                painter: ProgressArc(
-                                    _getCaloriesForMeal(_breakfastToday) +
-                                        _getCaloriesForMeal(_lunchToday) +
-                                        _getCaloriesForMeal(_dinnerToday),
-                                    _targetCalories,
-                                    Colors.blue[300]),
-                              ),
-                              CustomPaint(
-                                size: Size(300, 150),
-                                painter: ProgressArc(
-                                    _getCaloriesForMeal(_breakfastToday) + _getCaloriesForMeal(_lunchToday),
-                                    _targetCalories,
-                                    Colors.yellow[300]),
-                              ),
-                              CustomPaint(
-                                size: Size(300, 150),
-                                painter:
-                                    ProgressArc(_getCaloriesForMeal(_breakfastToday), _targetCalories, Colors.red[300]),
-                              ),
-                              Positioned.fill(
-                                  child: Align(
-                                      alignment: Alignment.center,
-                                      child: Padding(
-                                          padding: EdgeInsets.fromLTRB(10, 45, 15, 10),
-                                          child: Column(
-                                            children: [
-                                              RichText(
-                                                  text: new TextSpan(
-                                                      style: TextStyle(
-                                                          fontSize: 24,
-                                                          fontWeight: FontWeight.bold,
-                                                          color: Colors.grey[800]),
-                                                      children: <TextSpan>[
-                                                    new TextSpan(
-                                                        text: _totalCalories.toInt().toString(),
-                                                        style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
-                                                    new TextSpan(
-                                                        text: " kcal",
-                                                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal)),
-                                                  ])),
-                                              _getCalorieDifferenceText()
-                                            ],
-                                          ))))
-                            ],
-                          )),
+                      CalorieProgressBar(
+                          breakfastFoods: _breakfastToday,
+                          lunchFoods: _lunchToday,
+                          dinnerFoods: _dinnerToday,
+                          snackFoods: _snackToday,
+                          caloriesToday: _totalCalories,
+                          targetCalories: _targetCalories),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
                           RichText(
                               text: new TextSpan(
-                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[700]),
+                                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey[700]),
                                   children: <TextSpan>[
                                 new TextSpan(text: _totalProtein.toInt().toString() + "g"),
                                 new TextSpan(text: " Protein", style: TextStyle(fontWeight: FontWeight.normal)),
                               ])),
                           RichText(
                               text: new TextSpan(
-                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[700]),
+                                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey[700]),
                                   children: <TextSpan>[
                                 new TextSpan(text: _totalCarbs.toInt().toString() + "g"),
                                 new TextSpan(text: " Carbs", style: TextStyle(fontWeight: FontWeight.normal))
                               ])),
                           RichText(
                               text: new TextSpan(
-                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[700]),
+                                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey[700]),
                                   children: <TextSpan>[
                                 new TextSpan(text: _totalFat.toInt().toString() + "g"),
                                 new TextSpan(text: " Fat", style: TextStyle(fontWeight: FontWeight.normal))
@@ -275,45 +191,5 @@ class _Dairy_SectionState extends State<Dairy_Section> {
             }
           }),
     );
-  }
-}
-
-class ProgressArc extends CustomPainter {
-  bool isBackground;
-  double calories;
-  double targetCalories;
-  Color progressColor;
-  double arc;
-
-  ProgressArc(double calories, double targetCalories, Color progressColor) {
-    this.calories = calories;
-    this.targetCalories = targetCalories;
-    this.progressColor = progressColor;
-    this.isBackground = isBackground;
-
-    if (calories != null) {
-      this.arc = (calories / targetCalories) * (math.pi - 0.4);
-    }
-  }
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final rect = Rect.fromLTRB(0, 0, 300, 300);
-    final startAngle = -(math.pi - 0.2);
-    final sweepAngle = (arc == null || arc > math.pi - 0.4) ? math.pi - 0.4 : arc;
-    final userCenter = false;
-    final paint = Paint()
-      ..strokeCap = StrokeCap.round
-      ..color = progressColor
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 8;
-
-    canvas.drawArc(rect, startAngle, sweepAngle, userCenter, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    return true;
   }
 }
